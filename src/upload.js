7b066b9857a8ab7bc18bad983c6da00976349af5
darwin <darwin.apolinario@serino.com>
2022-01-01 15:15:09 +0800

    test

--- src/upload.js
+++ src/upload.js
@@ -3,16 +3,26 @@ const s3 = new AWS.S3();
 
 const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKETNAME;
 
-module.exports.handler = async (event) => {
-  console.log(event);
+const downsizeProfileImgForTweet = (img) => {
+  let imgBuffer = Buffer.from(img, "base64");
+  sharp(imgBuffer)
+    .resize(52, 52)
+    .toBuffer()
+    .then((data) => {
+      console.log(data, "99999999999999999999999");
+      console.log("success");
+      return data;
+    })
+    .catch((err) => console.log(`downisze issue ${err}`));
+};
 
+module.exports.handler = async (event) => {
   const response = {
     isBase64Encoded: false,
     statusCode: 200,
     body: JSON.stringify({ message: "Successfully uploaded file to S3" }),
   };
 
-  console.log("123");
   try {
     const parsedBody = JSON.parse(event.body);
     const base64File = parsedBody.file;
@@ -20,6 +30,8 @@ module.exports.handler = async (event) => {
       base64File.replace(/^data:image\/\w+;base64,/, ""),
       "base64"
     );
+    const decodedFile2 = downsizeProfileImgForTweet(decodedFile);
+
     const params = {
       Bucket: BUCKET_NAME,
       Key: `images/${new Date().toISOString()}.jpeg`,
@@ -27,7 +39,15 @@ module.exports.handler = async (event) => {
       ContentType: "image/jpeg",
     };
 
+    console.log(decodedFile2, "2222222222222222222222222222222222222222");
+    // const params2 = {
+    //   Bucket: BUCKET_NAME,
+    //   Key: `images/${new Date().toISOString()}.jpeg`,
+    //   Body: decodedFile2,
+    //   ContentType: "image/jpeg",
+    // };
     const uploadResult = await s3.upload(params).promise();
+    // const uploadResult2 = await s3.upload(params2).promise();
 
     response.body = JSON.stringify({
       message: "Successfully uploaded file to S3",
